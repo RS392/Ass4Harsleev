8a744c478a6a0fc2883dba24dfdeee383f96a560
unknown <lee.y.ashley@gmail.com>
2015-11-27 11:49:56 -0500

    added some, but not all comments

--- src/ass4/Element.java
+++ src/ass4/Element.java
@@ -1,3 +1,11 @@
+/* By: Radu Saghin and Ashley Lee (26663486)
+ * COMP 352, Assignment 4
+ * 
+ * The Hash table implemented will store objects of type Element, where each Element will two take string variables:
+ * 	1) The key string, so that it can be mapped to the hash table
+ * 	2) The value string which takes the actual value of what needs to be stored in the hash table
+ */
+
 package ass4;
 
 public class Element {
@@ -5,29 +13,37 @@ public class Element {
 	private String key;
 	private String value;
 	
+	// constructor
 	public Element(String key, String value) {
 		this.key = key;
 		this.value = value;
 	}
+	
 	public Element() {
 		
 	}
+	
+	// getter for the key
 	public String getKey() {
 		return key;
 	}
 
+	// setter for the key
 	public void setKey(String key) {
 		this.key = key;
 	}
 
+	// getter for the value
 	public String getValue() {
 		return value;
 	}
 
+	// setter for the value
 	public void setValue(String value) {
 		this.value = value;
 	}
 	
+	// toString that outputs the key number and the value stored into the hash table
 	public String toString() {
 		String s;
 		s = "key " + getKey() + " and value " + getValue();
